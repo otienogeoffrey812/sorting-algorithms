c72e78dbeb36ced5df187f76d49c7fe73f551728
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2024-12-15 11:38:13 +0300

    selection sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] array = { 1000, 12,4,23,673,45};
-        var bubbleSort = new BubbleSort();
-        bubbleSort.sort(array);
+        SortingAlgorithms sort = new SortingAlgorithms();
+        sort.selectionSort(array);
         System.out.println(Arrays.toString(array));
 
     }
---
+++ src/main/java/org/example/SortingAlgorithms.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class SortingAlgorithms {
+    public void bubbleSort(int[] array){
+
+        boolean isSorted;
+        for (int i = 0; i < array.length; i++) {
+            isSorted = true;
+            // Inner loop goes only up to the unsorted portion of the array -- last items are sorted
+            for (int j = 0; j < array.length - 1 - i; j++) {
+//            for (int j = 0; j < array.length - 1; j++) {
+                if(array[j] > array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if(isSorted) return;
+        }
+    }
+    public void selectionSort(int[] array){
+        //Find the smallest element in the unsorted part of the array.
+        //Swap it with the first element of the unsorted part (which becomes the last element of the sorted part).
+        //Repeat the process until the whole array is sorted.
+        // Time Complexity - n (passes) * n (comparisons) = n^2
+
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+
+                if(array[minIndex] > array[j]){
+                    minIndex = j;
+                }
+            }
+
+            if(minIndex != i){
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
